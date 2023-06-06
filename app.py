e4d296132ea44cd7852e205fb122d843d0d07706
ivfloyd <113161118+ivfloyd@users.noreply.github.com>
2023-06-06 05:01:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,9 @@ import stat
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/home/ivo/roop2/roop/uploads'
-
-# Creando una cola para las solicitudes
 task_queue = queue.Queue()
-# Creando un diccionario para guardar el estado de las tareas
 task_status = {}
 
-# Esta funcion corre en un hilo separado para procesar las solicitudes en la cola
 def worker():
     while True:
         task = task_queue.get()
@@ -29,7 +25,6 @@ def worker():
         task_status[user_id] = output_path
         task_queue.task_done()
 
-# Iniciando el worker en un hilo separado
 threading.Thread(target=worker, daemon=True).start()
 
 @app.route('/')
@@ -45,7 +40,7 @@ def run_script(face_path, target_path, output_path):
         '--gpu'
     ]
     subprocess.run(cmd)
-    # Cambiar los permisos del archivo de salida para que todos los usuarios puedan leerlo
+    # Cambiar los permisos del archivo de salida
     os.chmod(output_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
 
 @app.route('/upload', methods=['POST'])
@@ -74,9 +69,9 @@ def upload_file():
     output_filename = user_id + '_output_' + secure_filename(target_file.filename)
     output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
 
-    # Agrega la tarea a la cola
+    
     task_queue.put((user_id, face_path, target_path, output_path))
-    # Establece el estado inicial de la tarea
+
     task_status[user_id] = 'processing'
 
     return jsonify(user_id=user_id, output_file=output_filename)
